54053f999aef3435931e9dacb619ab1817bc5c91
Sirine <saidane.sirine2001@gmail.com>
2025-01-17 18:17:39 +0100

    video18

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.dummyText}>Welcome to React Native!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello world!!</Text>
+      <Button title="Click here!" onPress={() => alert("Button clicked")} />
     </View>
   );
 }
@@ -13,8 +15,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 16,
+    borderRadius: 20,
+    fontWeight: "bold",
+    fontSize: 20,
+    textAlign: "center",
   },
 });
