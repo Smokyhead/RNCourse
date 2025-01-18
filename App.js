83074feb0a051b6ec39ad9800946707568b81632
Sirine <saidane.sirine2001@gmail.com>
2025-01-19 00:12:04 +0100

    v24

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState([]);
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+    alert("Goal added!");
+  }
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputConatiner}>
-        <TextInput style={styles.textInput} placeholder="Your course goal!" />
-        <Button title="Add goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal" onPress={addGoalHandler}/>
       </View>
       <View style={styles.goalsContainer}>
-        <Text>List of goals</Text>
+        {courseGoals.map((goal) => 
+          <Text style={styles.goalText} key={goal}>{goal}</Text>
+        )}
       </View>
     </View>
   );
@@ -26,17 +46,43 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginBottom: 24,
     borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+    borderBottomColor: "#582f0e",
     flex: 1,
   },
   textInput: {
-    borderColor: "#cccccc",
+    borderColor: "#582f0e",
     borderWidth: 1,
     width: "75%",
     marginRight: 8,
     padding: 8,
   },
   goalsContainer: {
+    padding: 16,
     flex: 9,
+    marginBottom: 20,
+    borderRadius: 40,
+    backgroundColor: "#f5ebe0",
+    shadowColor: "black",
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    borderColor: "#582f0e",
+    borderWidth: 1,
+  },
+  goalText: {
+    paddingVertical: 12.5,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+    backgroundColor: "#d5bdaf",
+    borderRadius: 20,
+    fontFamily: "Arial",
+    fontSize: 18,
+    color: "#582f0e",
+    fontWeight: "bold",
+    borderColor: "#582f0e",
+    borderWidth: 1,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    shadowOffset: { width: 1, height: 1 },
   },
 });
